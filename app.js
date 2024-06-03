0c367d059894fd4ad6c21ac2bbc69d71cb7ff1b8
UnoRamos <150862634+UnoRamos@users.noreply.github.com>
2024-06-03 21:38:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/style.css', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'style.css'));
 });
 
